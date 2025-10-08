dec8f20463652bca89910293e55f07e455bf25bc
Alejandro Barata <alexbgdev@gmail.com>
2025-10-08 15:23:53 +0200

    FEAT: Setting react router

---
+++ app/root.tsx
@@ -0,0 +1,91 @@
+import {
+  isRouteErrorResponse,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useNavigation,
+  Links
+} from "react-router";
+
+import type { Route } from "./+types/root";
+// import './styles/index.sass';
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  const navigation = useNavigation();
+  // It Check if the user is navigation.
+  const isPending = navigation.state !== "idle";
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Links />
+      </head>
+      <body>
+        {isPending && <HydrateFallback />}
+        <main>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Outlet />
+  )
+}
+
+export function HydrateFallback() {
+  return (
+    <section
+      style={{
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        left: 0,
+        top: 0,
+        backgroundColor: '#001c33',
+        color: 'white'
+      }}
+    ><span style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translateX(-50%)'
+    }}>LOADING...</span>
+    </section>
+  );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack: string | undefined;
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details =
+      error.status === 404
+        ? "The requested page could not be found."
+        : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
+
+  return (
+    <main className="pt-16 p-4 container mx-auto">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,3 @@
+export default function Index() {
+  return <>Este es el index</>
+}
